ce13bb161482f2f219566432e48263a42eb36bd7
ALF <kazuhiro0517150@gmail.com>
2019-10-11 02:52:08 +0900

    Android/iOS共通部分の実装

---
+++ XMemo/XMemo/XMemo/Memo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMemo
+{
+    public class Memo
+    {
+        public DateTime Date
+        {
+            get;
+            set;
+        }
+
+        public string Subject
+        {
+            get;
+            set;
+        }
+
+        public string Text
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ XMemo/XMemo/XMemo/MemoHolder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMemo
+{
+    public class MemoHolder
+    {
+        public static MemoHolder Current
+        {
+            get;
+        } = new MemoHolder();
+
+        public Memo Memo
+        {
+            get;
+            set;
+        }
+    }
+}
